86e6fd39a9c025b6e06abc98d574b03f416aa728
PhanVuongTb <pdvuongtb@gmail.com>
2022-03-08 18:03:56 +0700

    upde3

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,29 @@ const arrSo: number[] = [1,2,3];
 const object : object = {};
 const arrObject: user[] = [{id: 1, name: "vuong"}, {id: 2, name: "vuong"},{id: 3, name: "vuong"}];
 
+export type User = {
+    id: number,
+    name: string,
+    age: number,
+    occupation: string
+};
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
